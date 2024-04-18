e1def688ff812f8a1be8527b1cadef1dfe9b3ac2
DREWSKIAC <115323436+DREWSKIAC@users.noreply.github.com>
2024-04-18 15:06:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -555,7 +555,7 @@ function emuLoop() {
 
     if (emuIsRunning) {
         if (config.powerSave) {
-            if (performance.now() - prevRunFrameTime < 32) {
+            if (performance.now() - prevRunFrameTime < 33) {
                 return
             }
         }
